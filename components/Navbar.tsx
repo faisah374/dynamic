1c40a98e1420b0fe003afe8cef68adea051d0be1
Faisal Hameed <faisalh374@gmail.com>
2024-10-22 11:00:52 +0500

    first commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import { NavbarProps } from '@/types/types'
 const Navbar:React.FC<NavbarProps> = ({name,href}) => {
   return (
     <div>
+      
         <li className='text-white bg-transparent hover:bg-teal-400 roundrd hover:text-black p=2'>
             <Link href={href}>{name}</Link>
 
